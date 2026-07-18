3688a9c7b168eba829587ac1b5f382b477a7fda1
Raju Putra <rajuputratzy@gmail.com>
2026-07-18 17:12:22 +0700

    add user session with refresh token and dbdocs

--- token/maker.go
+++ token/maker.go
@@ -5,7 +5,7 @@ import "time"
 // Maker is an interface that defines methods for creating and managing tokens.
 type Maker interface {
 	// CreateToken generates a new token for a given username and duration.
-	CreateToken(username string, duration time.Duration) (string, error)
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
 
 	// VerifyToken checks the validity of a token and returns the associated payload if valid.
 	VerifyToken(token string) (*Payload, error)
