6bf706b83a9cbc5868641c8478d663c097aa3a4d
Raju Putra <rajuputratzy@gmail.com>
2026-07-21 02:37:23 +0700

    Implement Redis task queue, integrate async worker, add sending email in go via email, implement email verification API in Go, write unite test for gRPC api with mock DB & Redis

--- worker/processor.go
+++ worker/processor.go
@@ -5,6 +5,13 @@ import (
 
 	"github.com/hibiken/asynq"
 	db "github.com/rajuputra/simplebank/db/sqlc"
+	"github.com/rajuputra/simplebank/mail"
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	QueueCritical = "critical"
+	QueueDefault  = "default"
 )
 
 type TaskProcessor interface {
@@ -15,17 +22,29 @@ type TaskProcessor interface {
 type RedisTaskProcessor struct {
 	server *asynq.Server
 	store  db.Store
+	mailer mail.EmailSender
 }
 
-func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store, mailer mail.EmailSender) TaskProcessor {
 	server := asynq.NewServer(
 		redisOpt,
-		asynq.Config{},
+		asynq.Config{
+			Queues: map[string]int{
+				QueueCritical: 10,
+				QueueDefault:  5,
+			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
+			Logger: NewLogger(),
+		},
 	)
 
 	return &RedisTaskProcessor{
 		server: server,
 		store:  store,
+		mailer: mailer,
 	}
 
 }
